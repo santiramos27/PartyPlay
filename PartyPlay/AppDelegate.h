66ab829d13097191da4c991bc4a428f3a1be0f39
Santino L Ramos <santinoramos@fb.com>
2021-07-14 23:47:58 -0400

    Initial Commit

---
+++ PartyPlay/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PartyPlay
+//
+//  Created by Santino L Ramos on 7/14/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PartyPlay/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PartyPlay
+//
+//  Created by Santino L Ramos on 7/14/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
